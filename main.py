f9a16782895960fcf10e2cf7d0e63c376d8bfa00
llorca-mfs <llorca.mfs@gmail.com>
2023-05-17 10:01:55 +0800

    cleaned up stuff

--- main.py
+++ main.py
@@ -15,42 +15,13 @@ bg = pygame.image.load("bg_256.png").convert_alpha()
 def main():
     gameRunning = True
 
-    isWalking_p1 = "False"
-    isWalking_p2 = "False"
-
     while gameRunning:
         screen.blit(bg, (0,0))
-        print("isWalking_p1 = {0}, isWalking_p2 = {1}".format(isWalking_p1, isWalking_p2))
 
         #PLAYER CONTROLS:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #player exits window
                 gameRunning = False
-
-            elif event.type == KEYDOWN:
-                #PLAYER 1
-                if event.key == K_a and isWalking_p1 == "False":
-                    isWalking_p1 = "Left"
-                if event.key == K_d and isWalking_p1 == "False":
-                    isWalking_p1 = "Right"
-                
-                #PLAYER 2
-                if event.key == K_j and isWalking_p2 == "False":
-                    isWalking_p2 = "Left"
-                if event.key == K_l and isWalking_p2 == "False":
-                    isWalking_p2 = "Right"
-            elif event.type == KEYUP:
-                #PLAYER 1
-                if event.key == K_a and not isWalking_p1 == "False":
-                    isWalking_p1 = "False"
-                if event.key == K_d and not isWalking_p1 == "False":
-                    isWalking_p1 = "False"
-
-                #PLAYER 2
-                if event.key == K_j and not isWalking_p2 == "False":
-                    isWalking_p2 = "False"
-                if event.key == K_l and not isWalking_p2 == "False":
-                    isWalking_p2 = "False"
             
             
         pygame.display.update()
